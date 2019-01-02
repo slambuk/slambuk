3afc30204e506df361c85a1c0efb981969dcad25
slambuk <46312521+slambuk@users.noreply.github.com>
2019-01-02 13:37:25 +0530

    urlshorten.js

---
+++ urlshorten.js
@@ -0,0 +1,82 @@
+var endpoint = "https://www.jsonstore.io/8ba4fd855086288421f770482e372ccb5a05d906269a34da5884f39eed0418a1";
+var hashhash = '';
+
+function geturl(){
+    /*var url = document.getElementById("urlinput").value;
+    var protocol_ok = url.startsWith("http://") || url.startsWith("https://") || url.startsWith("ftp://");
+    if(!protocol_ok){
+        newurl = "http://"+url;
+        return newurl;
+        }else{
+            return url;
+        }*/
+		
+	/*var urlParams;
+	(window.onpopstate = function () {
+		var match,
+			//pl     = /\+/g,  // Regex for replacing addition symbol with a space
+			search = /([^&=]+)=?([^&]*)/g,
+			decode = function (s) { return decodeURIComponent(s); },
+			query  = window.location.search.substring(1);
+			//query = '?i=main&mode=front&sid=de8d49b78a85a322c4155015fdce22c4&enc=+Hello%20&empty';
+
+		urlParams = {};
+		while (match = search.exec(query))
+		   urlParams[decode(match[1])] = decode(match[2]);
+	})();
+	var url = urlParams["l"];
+	return url;*/
+	
+	var url = document.getElementById("copylink").innerHTML;
+	return url;
+}
+
+function getrandom() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 5; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+}
+
+function genhash(){
+    if (window.location.hash == ""){
+        window.location.hash = getrandom();
+    }
+	//hashhash = getrandom();
+}
+
+function send_request(url) {
+    this.url = url;
+    $.ajax({
+        'url': endpoint + "/" + window.location.hash.substr(1),
+		//'url': endpoint + "/" + hashhash,
+        'type': 'POST',
+        'data': JSON.stringify(this.url),
+        'dataType': 'json',
+        'contentType': 'application/json; charset=utf-8'
+})
+}
+
+function shorturl(){
+    var longurl = geturl();
+    genhash();
+    send_request(longurl);
+	document.getElementById("surl").value = window.location.href;
+	//document.getElementById("surl").innerHTML = window.location.href + '#' + hashhash;
+}
+
+var hashh = window.location.hash.substr(1)
+
+if (window.location.hash != "") {
+    $.getJSON(endpoint + "/" + hashh, function (data) {
+        data = data["result"];
+
+        if (data != null) {
+            window.location.href = data;
+			//document.getElementById("surl").innerHTML = window.location.href;
+        }
+
+    });
+}
