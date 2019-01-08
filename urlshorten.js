f12c417e68c7c45d5db32cd4d2ba6194d8c21cac
slambuk <46312521+slambuk@users.noreply.github.com>
2019-01-08 23:26:16 +0530

    urlshorten.js v5

--- urlshorten.js
+++ urlshorten.js
@@ -35,7 +35,7 @@ function getrandom() {
     var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    for (var i = 0; i < 5; i++)
+    for (var i = 0; i < 7; i++)
         text += possible.charAt(Math.floor(Math.random() * possible.length));
     return text;
 }
